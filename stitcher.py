fdda82ca9138877055028899eef344fd88d21d8c
II_Zakiryanov <II_Zakirianov@bnipi.rosneft.ru>
2023-10-06 22:36:33 +0300

    Обновил логику Вынес все варьируемые параметры в отдельный файл Обновил ридми

--- stitcher.py
+++ stitcher.py
@@ -1,12 +1,12 @@
-from stitching import Stitcher
 import os
-import cv2
+import random
 import random
 import numpy as np
-import random
+import cv2
+from stitching import Stitcher
+from set_params import photos_path, seed, params, intersection, intermediate_res
 
-# считаем фотографии
-photos_path = r'photos'
+# подготовим данные
 bottom, top = [], []
 dim_arr = np.array(list(map(lambda x: x.split('_')[1], os.listdir(photos_path))))
 bottom_len, top_len = np.unique(dim_arr, return_counts=True)[1]
@@ -18,53 +18,43 @@ for i in range(1, shape):
         top.append(os.path.join(photos_path, f'{i}_2.jpg'))
 
 # зафиксируем сиды
-seed = 1
 random.seed(seed)
 np.random.seed(seed)
 cv2.setRNGSeed(seed)
 
-# объединим фотографии по горизонтали
-images = []
+def stitch(imgs, params):
+    stitcher = Stitcher(**params)
+    stitched_img = stitcher.stitch(imgs)
+    return stitched_img
+
+# объединим фотографии по горизонтали разделив пополам
+parts = []
 for num, i in enumerate([bottom, top]):
-    stitcher = Stitcher(medium_megapix=1, confidence_threshold=0.8, crop=True)
-    panorama = stitcher.stitch(i)
-    panorama = cv2.rotate(panorama, cv2.ROTATE_90_CLOCKWISE)
+    part = len(i) // 2
+    for j in range(0, len(i), part):
+        panorama = stitch(i[j: j+part+intersection], params)
+        cv2.imwrite(f'{num}_{j}.png', panorama) if intermediate_res else 0
+        panorama = cv2.rotate(panorama, cv2.ROTATE_90_CLOCKWISE)
+        parts.append(panorama)
+
+# объединим разделенные пополам части
+images = []
+for i in range(2):
+    panorama = stitch([parts[i], parts[i+2]], params)
+    panorama = cv2.rotate(panorama, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    cv2.imwrite(f'{i}.png', panorama) if intermediate_res else 0
     images.append(panorama)
 
-# обновим сиды
-seed = 1000
-random.seed(seed)
-np.random.seed(seed)
-cv2.setRNGSeed(seed)
-
 # объединим фотографии по вертикали
-stitcher = Stitcher(confidence_threshold=0.9, crop=False)
-stitched_img = stitcher.stitch(images)
-stitched_img = cv2.rotate(stitched_img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+stitched_img = stitch(images, params)
+cv2.imwrite(f'preresult.png', stitched_img) if intermediate_res else 0
 
-# обработаем границы
-clear_img = stitched_img.copy()
-for k in range(3):
-    for y, i in enumerate(stitched_img[:, :, k]):
-        non_zeros = np.where(i != 0)[0]
-        zeros = np.where(i == 0)[0]
-        if len(non_zeros) < stitched_img.shape[1] * 0.5:
-            if y < (stitched_img.shape[0] / 2):
-                top_bound = y + 15
-            else:
-                bottom_bound = y - 15
-            continue
-        for x, j in enumerate(i):
-            if j == 0:
-                if x < (len(i) / 2):
-                    clear_img[y, zeros[-1], k] = i[non_zeros[-1]]
-                    zeros = zeros[:-1]
-                    non_zeros = non_zeros[:-1]
-            else:
-                break
-
-# удалим ненужные края
-crop_img = clear_img[top_bound:bottom_bound, np.mean(clear_img[:, :, 0] == 0, axis=0) < 0.01, :]
-
-# сохраним результат
-cv2.imwrite('result.png', crop_img)
+# разделим пополам и объединим, нужно для точного совпадения краев
+half_shape = stitched_img.shape[1] // 2
+left_img = cv2.flip(stitched_img[:, :half_shape, :], 1)
+right_img = cv2.flip(stitched_img[:, half_shape:, :], 1)
+try:
+    stitched_img = stitch([left_img, right_img], params)
+finally:
+    # сохраним результат
+    cv2.imwrite(f'result_{photos_path}.png', stitched_img)
